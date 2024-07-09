04b9bb53e84c4c7badea53be09ba7ecd39e0f8df
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2024-07-09 17:02:15 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# python-daily-weather-report
-Este repositório contém um web scraper em Python que coleta informações meteorológicas de um site de previsão do tempo e envia um e-mail diário com a previsão para o seu endereço de e-mail.
+# Python Daily Weather Report
+
+Este repositório contém um web scraper em Python que coleta informações 
+meteorológicas de um site de previsão do tempo e envia um e-mail diário com a 
+previsão para o seu endereço de e-mail.
+<!-- 
+## Funcionalidades
+
+- **Coleta de dados:**
+- **Envio de e-mail:**
+- **Personalização:**
+
+## Como usar
+
+1. **Clone do projeto no github:**
+   ```terminal
+   <comando>
+   ```
+
+2. **Instale as dependências:**
+
+3. **Editar o script:**
+   - `variavel_1`: descricao
+   - `variavel_2`: descricao
+   - `variavel_3`: descricao
+
+4. **Executar o script:**
+   ```terminal
+   <comando>
+   ```
+
+## Contribuições
+
+Contribuições são bem-vindas! Se você encontrar algum problema, tiver alguma 
+sugestão de melhoria ou quiser adicionar funcionalidades, por favor abra um 
+issue ou faça um pull request. -->
---
+++ app.py
@@ -0,0 +1,31 @@
+"""
+title: 'python-daily-weather-report'
+author: 'Elias Albuquerque'
+version: 'Python 3.12.0'
+created: '2024-07-09'
+update: '2024-07-09'
+"""
+
+# COLETAR PREVISÃO(url)
+# 1. Navegação Automática:
+# - Abrir um navegador e acessar um site de previsão do tempo(você pode escolher
+# o site)
+
+# 2. Coleta de Dados Meteorológicos:
+# - Extrair a temperatura atual.
+# - Extrair a condição do tempo atual (ex. ensolarado, nublado, etc.).
+# - Extrair a previsão para os próximos 3 dias (temperatura e condição do tempo).
+
+# 3. Tratamento e Formatação de Dados:
+# - Organizar os dados extraídos em um formato legível.
+
+# ENVIAR EMAIL(destinatario, assunto, corpo)
+# 4. Envio de E-mail:
+# - Configurar o envio de e-mails.
+# - Criar o conteúdo do e-mail com os dados meteorológicos coletados.
+# - Enviar o e-mail para um destinatário específico.(pode enviar para você mesmo
+# como teste)
+
+# VERIFICAR SE JA TEM AGENDAMENTO DO SCRIPT, SE NÃO, USAR O SISTEMA OP. PARA AGENDAR, SEJA WINDOWS OU LINUX, OU USANDO BIBLIOTECA PYTHON (QUE EU ACHO MENOS PROFISSIONAL)
+# 5. Automatização do Envio Diário:
+# - Agendar a execução do script para rodar diariamente em um horário específico.
