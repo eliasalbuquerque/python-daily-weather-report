833965a6b919f359c62391c91e5c6f7a37f22936
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2024-07-11 15:35:42 -0300

    update: add collect weather forecast
    ADDED FEATURES:
    
    * created the dictionary to collect the data;
    * get todays date and the day of the week;
    * translate the days of the week into Portuguese;
    * access the website and collect the weather forecast data;
    * add the data to the dictionary;
    * handle errors and document the code.

--- README.md
+++ README.md
@@ -3,6 +3,35 @@
 Este repositório contém um web scraper em Python que coleta informações 
 meteorológicas de um site de previsão do tempo e envia um e-mail diário com a 
 previsão para o seu endereço de e-mail.
+
+**Desenvolvimento:**
+
+1. Coleta de Dados Meteorológicos:
+   - [x] Criar dicionário para armazenar os dados;
+   - [x] Pegar a data de hoje com o dia da semana;
+   - [x] Navegar até o site [msn.com/clima](https://www.msn.com/pt-br/clima/forecast/) e coletar os dados;
+   - [x] Salvar dados no dicionário;
+   - [x] Documentar o trecho do código.
+2. Tratamento e Formatação de Dados:
+   - [ ] Criar a mensagem para o envio do e-mail;
+   - [ ] Adicionar os dados do dicionário no corpo da mensagem;
+   - [ ] Documentar o trecho do código.
+3. Envio de E-mail:
+   - [ ] Pesquisar bibliotecas de envio de e-mail com Python;
+   - [ ] Enviar e-mail com a mensagem como corpo do e-mail;
+   - [ ] Documentar o trecho do código.
+4. Automatização do Envio Diário
+   - [ ] Automatizar utilizando biblioteca Python;
+   - [ ] Automatizar utilizando configurações do sistema operacional;
+   - [ ] Documentar o trecho do código.
+5. Update o README.md:
+   - [ ] Funcionalidades;
+   - [ ] Como usar:
+     - [ ] clonar o projeto;
+     - [ ] instalar as dependências;
+     - [ ] como editar o script;
+     - [ ] como executar o script.
+
 <!-- 
 ## Funcionalidades
 
@@ -28,9 +57,10 @@ previsão para o seu endereço de e-mail.
    ```terminal
    <comando>
    ```
+-->
 
 ## Contribuições
 
 Contribuições são bem-vindas! Se você encontrar algum problema, tiver alguma 
 sugestão de melhoria ou quiser adicionar funcionalidades, por favor abra um 
-issue ou faça um pull request. -->
+issue ou faça um pull request. 
--- app.py
+++ app.py
@@ -1,179 +1,79 @@
-# """
-# title: 'python-daily-weather-report'
-# author: 'Elias Albuquerque'
-# version: 'Python 3.12.0'
-# created: '2024-07-09'
-# update: '2024-07-10'
-# """
-
-
-# from time import sleep
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options as ChromeOptions
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from webdriver_manager.chrome import ChromeDriverManager
-# import datetime
-
-
-# def collect_weather_forecast(url):
-#     """Coleta dados de previsão do tempo."""
-
-#     driver = webdriver.Chrome()
-#     driver.get(url)
-#     sleep(2)
-
-#     try:
-#         wait = WebDriverWait(driver, 15)
-
-#         # Adaptar ao XPATH específico do site
-#         xp_current_temperature = '//div[@class="u1SummaryTextContainer-DS-EntryPoint1-1"]//a[@title]'
-#         xp_current_weather = '//div[@class="u1SummaryCaptionCompact-DS-EntryPoint1-1"]'
-#         xp_future_max_temperature = '//div[@class="topTemp-DS-EntryPoint1-1 temp-DS-EntryPoint1-1"]'
-#         xp_future_min_temperature = '//div[@class="temp-DS-EntryPoint1-1"]'
-#         xp_future_weather_conditions = '//div[@class="iconTempPartContainer-DS-EntryPoint1-1"]/img[@class="iconTempPartIcon-DS-EntryPoint1-1"]'
-
-#         current_temperature = wait.until(
-#             EC.visibility_of_element_located((
-#                 By.XPATH, xp_current_temperature))).get_attribute("title")
-
-#         current_weather = wait.until(
-#             EC.visibility_of_element_located((
-#                 By.XPATH, xp_current_weather))).text
-
-#         # Obtendo lista de elementos
-#         future_max_temperatures = wait.until(
-#             EC.visibility_of_all_elements_located((By.XPATH, xp_future_max_temperature)))
-#         future_min_temperatures = wait.until(
-#             EC.visibility_of_all_elements_located((By.XPATH, xp_future_min_temperature)))
-#         future_weather_conditions = wait.until(
-#             EC.visibility_of_all_elements_located((By.XPATH, xp_future_weather_conditions)))
-
-#         # Obter a data e hora atuais
-#         date = datetime.datetime.now()
-#         current_day = date.strftime('%d')
-#         current_weekday = date.strftime('%A')
-
-#         weather_forecast = {
-#             'today': {
-#                 'day': current_day,
-#                 'weekday': current_weekday,
-#                 'temperature': current_temperature,
-#                 'condition': current_weather
-#             }
-#         }
-
-#         days = ['tomorrow', 'day_after', 'next_day']
-#         for i, day in enumerate(days):
-#             date += datetime.timedelta(days=1)
-#             next_day = date.strftime('%d')
-#             next_weekday = date.strftime('%A')
-
-#             # Incrementando o índice dos elementos futuros
-#             index = i + 1  # Índice inicial é 0
-
-#             weather_forecast[day] = {
-#                 'day': next_day,
-#                 'weekday': next_weekday,
-#                 'maximum': future_max_temperatures[index].text,
-#                 'minimum': future_min_temperatures[index].text,
-#                 'condition': future_weather_conditions[index].get_attribute("title")
-#             }
-
-#         return weather_forecast
-
-#     except Exception as e:
-#         print(f'Data collection error: {e}')
-
-#     finally:
-#         driver.quit()
-
-# try:
-#     wait = WebDriverWait(driver, 15)
-
-#     # Adaptar ao XPATH específico do site
-#     xp_current_temperature = '//div[@class="u1SummaryTextContainer-DS-EntryPoint1-1"]//a[@title]'
-#     xp_current_weather = '//div[@class="u1SummaryCaptionCompact-DS-EntryPoint1-1"]'
-#     xp_future_max_temperature = '//div[@class="topTemp-DS-EntryPoint1-1 temp-DS-EntryPoint1-1"]'
-#     xp_future_min_temperature = '//div[@class="temp-DS-EntryPoint1-1"]'
-#     xp_future_weather_conditions = '//div[@class="iconTempPartContainer-DS-EntryPoint1-1"]/img[@class="iconTempPartIcon-DS-EntryPoint1-1"]'
-
-#     current_temperature = wait.until(
-#         EC.visibility_of_element_located((
-#             By.XPATH, xp_current_temperature))).get_attribute("title")
-
-#     current_weather = wait.until(
-#         EC.visibility_of_element_located((
-#             By.XPATH, xp_current_weather))).text
-
-#     future_max_temperature = wait.until(
-#         EC.visibility_of_element_located((
-#             By.XPATH, xp_future_max_temperature))).text
-
-#     future_min_temperature = wait.until(
-#         EC.visibility_of_element_located((
-#             By.XPATH, xp_future_min_temperature))).text
-
-#     future_weather_conditions = wait.until(
-#         EC.visibility_of_element_located((
-#             By.XPATH, xp_future_weather_conditions))).get_attribute("title")
-
-#     # Obter a data e hora atuais
-#     date = datetime.datetime.now()
-#     current_day = date.strftime('%d')
-#     current_weekday = date.strftime('%A')
-
-#     weather_forecast = {
-#         'today': {
-#         'day': current_day,
-#         'weekday': current_weekday,
-#         'temperature': current_temperature,
-#         'condition': current_weather
-#         }
-#     }
-
-#     days = ['tomorrow', 'day_after', 'next_day']
-#     for i, day in enumerate(days):
-#         date += datetime.timedelta(days=1)
-#         next_day = date.strftime('%d')
-#         next_weekday = date.strftime('%A')
-#         weather_forecast[day] = {
-#             'day': next_day,
-#             'weekday': next_weekday,
-#             'maximum': future_max_temperature,
-#             'minimum': future_min_temperature,
-#             'condition': future_weather_conditions
-#         }
-
-#     return weather_forecast
-
-# except Exception as e:
-#     print(f'Data collection error: {e}')
-
-# finally:
-#     driver.quit()
 """
 title: 'python-daily-weather-report'
 author: 'Elias Albuquerque'
 version: 'Python 3.12.0'
 created: '2024-07-09'
-update: '2024-07-10'
+update: '2024-07-11'
 """
 
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import datetime
 
 
 def collect_weather_forecast(url):
-    """Coleta dados de previsão do tempo."""
+    """
+    Gets the weather forecast from a URL.
 
+    This function uses Selenium to automate navigation to the weather forecast 
+    website and collect relevant information, including temperature, weather 
+    conditions, and additional data such as maximum and minimum for the next 
+    few days. The data is organized into a dictionary for easy access.
+
+    Args:
+        url (str): URL of the weather forecast website.
+
+    Returns:
+        dict: Dictionary containing the collected weather forecast, organized 
+        by day and relevant data such as: temperature, condition, maximum, 
+        minimum, day, and day of the week.
+        In case of error, returns None.
+    """
+    
+    # Create dictionary to collect data
+    weather_data = {}
+    days = ['today', 'tomorrow', 'day_after', 'next_day']
+
+    for day in days:
+        if day == 'today':
+            weather_data[day] = {
+                'day': None,
+                'weekday': None,
+                'temperature': None,
+                'condition': None
+            }
+        else:
+            weather_data[day] = {
+                'day': None,
+                'weekday': None,
+                'maximum': None,
+                'minimum': None,
+                'condition': None
+            }
+
+    # Get current date and time
+    date = datetime.datetime.now()
+    current_day = date.strftime('%d')
+    current_weekday = date.strftime('%A')
+
+    def translate_weekdays(weekday):
+        weekdays_ptbr = {
+            'Monday': 'Segunda-feira',
+            'Tuesday': 'Terça-feira',
+            'Wednesday': 'Quarta-feira',
+            'Thursday': 'Quinta-feira',
+            'Friday': 'Sexta-feira',
+            'Saturday': 'Sábado',
+            'Sunday': 'Domingo',
+        }
+
+        weekday_ptbr = weekdays_ptbr[weekday]
+        return weekday_ptbr
+
+    # Accessing the site
     driver = webdriver.Chrome()
     driver.get(url)
     sleep(2)
@@ -181,64 +81,46 @@ def collect_weather_forecast(url):
     try:
         wait = WebDriverWait(driver, 15)
 
-        # Adaptar ao XPATH específico do site
+        # Adapting to site-specific XPATH:
         xp_current_temperature = '//div[@class="u1SummaryTextContainer-DS-EntryPoint1-1"]//a[@title]'
         xp_current_weather = '//div[@class="u1SummaryCaptionCompact-DS-EntryPoint1-1"]'
+        xp_future_max_temperature = '//div[@class="topTemp-DS-EntryPoint1-1 temp-DS-EntryPoint1-1"]'
+        xp_future_min_temperature = '//div[@class="temp-DS-EntryPoint1-1"]'
+        xp_future_weather_conditions = '//div[@class="iconTempPartContainer-DS-EntryPoint1-1"]/img[@class="iconTempPartIcon-DS-EntryPoint1-1"]'
 
         current_temperature = wait.until(
             EC.visibility_of_element_located((
                 By.XPATH, xp_current_temperature))).get_attribute("title")
-        # print(f"Current Temperature: {current_temperature}")
 
         current_weather = wait.until(
             EC.visibility_of_element_located((
                 By.XPATH, xp_current_weather))).text
-        # print(f"Current Weather: {current_weather}")
 
-        # Obter a data e hora atuais
-        date = datetime.datetime.now()
-        current_day = date.strftime('%d')
-        current_weekday = date.strftime('%A')
+        future_max_temperature = driver.find_elements(
+            By.XPATH, xp_future_max_temperature)
 
-        weather_forecast = {
-            'today': {
-                'day': current_day,
-                'weekday': current_weekday,
-                'temperature': current_temperature,
-                'condition': current_weather
-            }
-        }
+        future_min_temperature = driver.find_elements(
+            By.XPATH, xp_future_min_temperature)
 
-        days = ['tomorrow', 'day_after', 'next_day']
-        for i, day in enumerate(days):
-            date += datetime.timedelta(days=1)
-            next_day = date.strftime('%d')
-            next_weekday = date.strftime('%A')
-        
-            xp_future_max_temperature = '//div[@class="topTemp-DS-EntryPoint1-1 temp-DS-EntryPoint1-1"][i]'
-            xp_future_min_temperature = '//div[@class="temp-DS-EntryPoint1-1"][i]'
-            xp_future_weather_conditions = '//div[@class="iconTempPartContainer-DS-EntryPoint1-1"]/img[@class="iconTempPartIcon-DS-EntryPoint1-1"][i]'
+        future_weather_condition = driver.find_elements(
+            By.XPATH, xp_future_weather_conditions)
 
-            # Recolher dados para cada dia futuro
-            future_max_temperature = wait.until(
-                EC.visibility_of_all_elements_located((
-                    By.XPATH, xp_future_max_temperature))).text
-            future_min_temperature = wait.until(
-                EC.visibility_of_all_elements_located((
-                    By.XPATH, xp_future_min_temperature))).text
-            future_weather_condition = wait.until(
-                EC.visibility_of_all_elements_located((
-                    By.XPATH, xp_future_weather_conditions))).get_attribute("title")
+        # Adding values to the dictionary: *Weekdays pt-br and Temperature in °C
+        weather_data['today']['day'] = current_day
+        weather_data['today']['weekday'] = translate_weekdays(current_weekday)
+        weather_data['today']['temperature'] = current_temperature
+        weather_data['today']['condition'] = current_weather
 
-            weather_forecast[day] = {
-                'day': next_day,
-                'weekday': next_weekday,
-                'maximum': future_max_temperature,
-                'minimum': future_min_temperature,
-                'condition': future_weather_condition
-            }
+        for i, day in enumerate(days[1:]):
+            weather_data[day]['day'] = str(int(current_day) + i + 1)
+            weather_data[day]['weekday'] = translate_weekdays((
+                datetime.date.today() + datetime.timedelta(days=i+1)
+            ).strftime('%A'))
+            weather_data[day]['maximum'] = future_max_temperature[i].text + 'C'
+            weather_data[day]['minimum'] = future_min_temperature[i].text + 'C'
+            weather_data[day]['condition'] = future_weather_condition[i + 1].get_attribute("title")
 
-        return weather_forecast
+        return weather_data
 
     except Exception as e:
         print(f'Data collection error: {e}')
@@ -246,12 +128,8 @@ def collect_weather_forecast(url):
     finally:
         driver.quit()
 
-
 # 3. Tratamento e Formatação de Dados:
 # - Organizar os dados extraídos em um formato legível.
-
-# DADOS:
-#
 
 # MENSAGEM:
 """
@@ -280,9 +158,15 @@ mín. <Z°C>         mín. <Z°C>         mín. <Z°C>
 # 5. Automatização do Envio Diário:
 # - Agendar a execução do script para rodar diariamente em um horário
 #   específico.
+
 if __name__ == '__main__':
     # url = 'https://facebook.com' # teste
-    url = 'https://www.msn.com/en-us/weather/forecast/in-Socorro%2C-S%C3%A3o-Paulo,SP?loc=eyJsIjoiU29jb3JybywgU8OjbyBQYXVsbyIsInIiOiJTUCIsImMiOiJCcmF6aWwiLCJpIjoiQlIiLCJnIjoiZW4tdXMiLCJ4IjoiLTQ2LjcwOTMwMDk5NDg3MzA1IiwieSI6Ii0yMy42ODQwMDAwMTUyNTg3OSJ9&weadegreetype=C&ocid=winp2fphotkey&cvid=f09231fded124191bc88bb4d161db80e'
+
+    # url = 'https://www.msn.com/en-us/weather/forecast/in-Socorro%2C-S%C3%A3o-Paulo,SP?loc=eyJsIjoiU29jb3JybywgU8OjbyBQYXVsbyIsInIiOiJTUCIsImMiOiJCcmF6aWwiLCJpIjoiQlIiLCJnIjoiZW4tdXMiLCJ4IjoiLTQ2LjcwOTMwMDk5NDg3MzA1IiwieSI6Ii0yMy42ODQwMDAwMTUyNTg3OSJ9&weadegreetype=C&ocid=winp2fphotkey&cvid=f09231fded124191bc88bb4d161db80e'
+
+    # url = 'https://www.msn.com/en-us/weather/forecast/'
+    
+    url = 'https://www.msn.com/pt-br/clima/forecast/'
 
     weather_forecast = collect_weather_forecast(url)
     print(weather_forecast)
